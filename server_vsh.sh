bf48f4b8676623b502814cb96be25b02abe71866
Kevin Personnic <personnic.kevin@gmail.com>
2014-05-18 18:55:34 +0200

    add server files

---
+++ server_vsh.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#set -x
+
+#########################################
+#										#
+#	@Author : Kevin Personnic			#
+#	@Date   : 17/05/2014				#
+#	@Synop  : Server vsh, gestion arch 	#
+#										#
+#########################################
+
+function usage {
+	echo -e "Usage : $0 {PORT}"
+	exit 1
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+
+
+for((;;));
+do
+	nc -vlp $1 -e controller_vsh;
+done
